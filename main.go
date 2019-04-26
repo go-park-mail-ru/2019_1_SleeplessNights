f1e6e973b027d985e44e2817ca59cee2659c8656
Uimin Maksim <uimin1maksim@yandex.ru>
2019-04-26 14:28:02 +0300

    Command line flags provided

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	log "github.com/go-park-mail-ru/2019_1_SleeplessNights/meta/logger"
+	"os"
 )
 
 var logger *log.Logger
@@ -15,13 +17,33 @@ func main() {
 	//TODO MAKE SECRET MANAGER ?
 	//TODO DELETE DATA CREATOR
 
+	conf := flag.String("-conf","DEV", "Sets the configuration to: DEV (default), TEST, LOCAL or PROD")
+	verb := flag.Bool("v", false, "Shows more info during the execution")
 
+	flag.Parse()
 
-	/*err := exec.Command("go","run", "auth_microservice/main.go").Run()
+	configs := make(map[string]struct{})
+	configs["DEV"]   = struct{}{}
+	configs["TEST"]  = struct{}{}
+	configs["LOCAL"] = struct{}{}
+	configs["PROD"]  = struct{}{}
+	if _, found := configs[*conf]; !found {
+		logger.Fatal("Unexpected configuration key", *conf, "provided")
+	}
+	err := os.Setenv("CONFIG", *conf)
+	if err != nil {
+		logger.Fatal("Failed to set configuration env variable", err)
+	}
+
+	if *verb {
+		logger.Debug("Setting config params to more verbose")
+	}
+
+	/*err = exec.Command("go run auth_microservice/main.go").Run()
 	if err != nil {
 		logger.Fatal("Can't run auth microservice:", err)
-	}
-	err = exec.Command("go","run", "main_microservice/main.go").Run()
+	}*/
+	/*err = exec.Command("go","run", "main_microservice/main.go").Run()
 	if err != nil {
 		logger.Fatal("Can't run main microservice")
 	}*/
