d6cfdc42bbe690afbd42b6bef94ed347ffa8cade
mac <a.larutin@yandex.ru>
2019-04-27 12:00:44 +0300

    fixed db dump

--- chat_microservice/database/sql.sql
+++ chat_microservice/database/sql.sql
@@ -5,6 +5,8 @@
 -- Dumped from database version 10.7 (Ubuntu 10.7-0ubuntu0.18.04.1)
 -- Dumped by pg_dump version 10.7 (Ubuntu 10.7-0ubuntu0.18.04.1)
 
+CREATE SCHEMA public;
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
