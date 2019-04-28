7cfba3fbeeedbe08850b06945768ba36f6652d1b
DragonF0rm <uimin1maksim@yandex.ru>
2019-04-28 14:12:07 +0000

    Config fixrd

--- scripts/start-consul.sh
+++ scripts/start-consul.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-consul agent -dev -config-dir=../consul.d -node=machine -ui -http-port 8010
+consul agent -dev -config-dir=../consul.d -node=machine -ui -http-port 8010 -client 0.0.0.0
