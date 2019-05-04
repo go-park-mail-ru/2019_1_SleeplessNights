c1bf7c02b870b10fe7c8a3aa125326a870c7bd54
mac <a.larutin@yandex.ru>
2019-05-04 19:04:34 +0300

    nice try

--- shared/errors/errors.go
+++ shared/errors/errors.go
@@ -3,7 +3,7 @@ package errors
 import "errors"
 
 var (
-	DataBaseUniqueViolation = errors.New("ERROR: unique_violation (SQLSTATE 23505)")
+	DataBaseUniqueViolation = errors.New("rpc error: code = Unknown desc = ERROR: unique_violation (SQLSTATE 23505)")
 	DataBaseNoDataFound     = errors.New("ERROR: no_data_found (SQLSTATE P0002)")
 	AuthWrongPassword       = errors.New("authentication failed because of wong password")
 )
