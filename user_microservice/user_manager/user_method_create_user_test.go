aed5e6e265466879671cdf1ab4577a1023eb3a5f
mac <a.larutin@yandex.ru>
2019-05-08 16:32:54 +0300

    added users_test in user_microservice

---
+++ user_microservice/user_manager/user_method_create_user_test.go
@@ -0,0 +1,60 @@
+package user_manager_test
+
+import (
+	"github.com/go-park-mail-ru/2019_1_SleeplessNights/shared/errors"
+	"github.com/go-park-mail-ru/2019_1_SleeplessNights/shared/services"
+	"github.com/go-park-mail-ru/2019_1_SleeplessNights/user_microservice/database"
+	"github.com/go-park-mail-ru/2019_1_SleeplessNights/user_microservice/user_manager"
+	"golang.org/x/net/context"
+	"testing"
+)
+
+func TestCreateUserSuccessful(t *testing.T) {
+
+	err := database.GetInstance().CleanerDBForTests()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	oldUser := services.NewUserData{
+		Email:    "test@test.com",
+		Nickname: "test",
+	}
+
+	var ctx context.Context
+
+	newUser, err := user_manager.GetInstance().CreateUser(ctx, &oldUser)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	if newUser.Email != oldUser.Email || newUser.Nickname != oldUser.Nickname {
+		t.Errorf("DB returned wrong user:\ngot %v, %v\nwant %v, %v\n",
+			newUser.Email, newUser.Nickname, oldUser.Email, oldUser.Nickname)
+	}
+}
+
+func TestCreateUserUnsuccessful(t *testing.T) {
+
+	oldUser := services.NewUserData{
+		Email:    "test@test.com",
+		Nickname: "test",
+	}
+
+	var ctx context.Context
+	expected := errors.DataBaseUniqueViolationShort
+
+	_, err := user_manager.GetInstance().CreateUser(ctx, &oldUser)
+	if err == nil {
+		t.Errorf("DB didn't return any error")
+		return
+	} else if err.Error() != expected.Error() {
+		t.Errorf("DB returned wrong error:\ngot %v\nwant %v\n",
+			err.Error(), expected)
+	}
+
+	err = database.GetInstance().CleanerDBForTests()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
