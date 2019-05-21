71f5b4fd8e561c983fe289513a35420aa357f62f
mac <a.larutin@yandex.ru>
2019-05-21 03:01:02 +0300

    makeup

--- chat_microservice/router/mux.go
+++ chat_microservice/router/mux.go
@@ -2,18 +2,9 @@ package router
 
 import (
 	"github.com/go-park-mail-ru/2019_1_SleeplessNights/chat_microservice/handlers"
-	log "github.com/go-park-mail-ru/2019_1_SleeplessNights/shared/logger"
 	"github.com/go-park-mail-ru/2019_1_SleeplessNights/shared/middleware"
 	"github.com/gorilla/mux"
-	"github.com/sirupsen/logrus"
 )
-
-var logger *log.Logger
-
-func init() {
-	logger = log.GetLogger("Router")
-	logger.SetLogLevel(logrus.TraceLevel)
-}
 
 func GetRouter() (router *mux.Router) {
 	router = mux.NewRouter()
