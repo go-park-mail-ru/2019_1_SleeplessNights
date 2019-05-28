d0d4e14704ffcd4574eba820cdd8b9c7f0103bd1
maxp007 <32578978+maxp007@users.noreply.github.com>
2019-05-28 16:22:13 +0300

    fix

--- game_microservice/player/factory/channel_player.go
+++ game_microservice/player/factory/channel_player.go
@@ -1,26 +1,26 @@
 package factory
 
 import (
-	"github.com/go-park-mail-ru/2019_1_SleeplessNights/TheGame/messge"
+	"github.com/go-park-mail-ru/2019_1_SleeplessNights/game_microservice/message"
 )
 
-type ChannelPlayerLogic func(id uint64, in, out *chan messge.Message, args ...interface{})()//id - просто идентификатор для логов
+type ChannelPlayerLogic func(id uint64, in, out *chan message.Message, args ...interface{})()//id - просто идентификатор для логов
 
 type channelPlayer struct {
 	//С помощью этой структуры будем делать игрока по вебсокету на фабрике
 	//Она уже реализует интерфейс Player, поэтому нам нужно будет просто сделатьинстанс этой структуры
 	work ChannelPlayerLogic
 	id   uint64
-	in   chan messge.Message
-	out  chan messge.Message
+	in   chan message.Message
+	out  chan message.Message
 }
 
-func (chanPlayer *channelPlayer) Send(msg messge.Message) (err error) {
+func (chanPlayer *channelPlayer) Send(msg message.Message) (err error) {
 	chanPlayer.out <- msg
 	return nil
 }
 
-func (chanPlayer *channelPlayer) Subscribe() chan messge.Message {
+func (chanPlayer *channelPlayer) Subscribe() chan message.Message {
 	return chanPlayer.in
 }
 
@@ -34,7 +34,7 @@ func (chanPlayer *channelPlayer) UID() uint64 {
 
 func (chanPlayer *channelPlayer) Close() {
 	logger.Infof("Player %d closed the connection", chanPlayer.id)
-	chanPlayer.in <- messge.Message{Title: messge.Leave}
+	chanPlayer.in <- message.Message{Title: message.Leave}
 	close(chanPlayer.in)
 	close(chanPlayer.out)
 }
