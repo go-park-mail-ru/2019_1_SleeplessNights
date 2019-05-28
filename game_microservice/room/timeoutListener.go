084f3517651c7063dd92de8baf4a30f18251976b
mac <a.larutin@yandex.ru>
2019-05-28 15:20:16 +0300

    added easyjson in game

--- game_microservice/room/timeoutListener.go
+++ game_microservice/room/timeoutListener.go
@@ -1,22 +1,22 @@
 package room
 
 import (
-	"encoding/json"
 	"github.com/go-park-mail-ru/2019_1_SleeplessNights/game_microservice/message"
 )
 
 func (r *Room) AnswerTimerFunc() {
 	logger.Info("AnswerTimerFunc Timer Has Expired before answer was given")
 
-	result, err := json.Marshal(message.Message{
+	msg := message.Message{
 		Title:   message.ClientAnswer,
 		Payload: message.Answer{AnswerId: -1},
-	})
+	}
+	result, err := msg.MarshalJSON()
 	if err != nil {
 		logger.Error("startAnswerTimeChecker Error trying to marshall Answer response")
 	}
 	var answer message.Message
-	err = json.Unmarshal(result, &answer)
+	err = answer.UnmarshalJSON(result)
 	if err != nil {
 		logger.Error("startAnswerTimeChecker Error trying to UnMarshal Answer response")
 	}
@@ -24,18 +24,18 @@ func (r *Room) AnswerTimerFunc() {
 }
 
 func (r *Room) GoToTimerFunc() {
-
 	logger.Info("GoToTimerFunc Timer Has Expired before answer was given")
 
-	result, err := json.Marshal(message.Message{
+	msg := message.Message{
 		Title:   message.GoTo,
 		Payload: message.Coordinates{X: -1, Y: -1},
-	})
+	}
+	result, err := msg.MarshalJSON()
 	if err != nil {
 		logger.Error("startGoToTimeChecker Error trying to marshall GoTo response")
 	}
 	var GoToMessage message.Message
-	err = json.Unmarshal(result, &GoToMessage)
+	err = GoToMessage.UnmarshalJSON(result)
 	if err != nil {
 		logger.Error("startGoToTimeChecker Error trying to UnMarshal GoTo response")
 	}
@@ -43,18 +43,18 @@ func (r *Room) GoToTimerFunc() {
 }
 
 func (r *Room) ChoosePackTimerFunc() {
-
 	logger.Info("ChoosePackTimerFunc Timer Has Expired before answer was given")
 
-	result, err := json.Marshal(message.Message{
+	msg := message.Message{
 		Title: message.NotDesiredPack,
 		Payload: message.PackID{PackId: -1},
-	})
+	}
+	result, err := msg.MarshalJSON()
 	if err != nil {
 		logger.Error("ChoosePackTimerFunc Error trying to marshall NotDesiredPack response")
 	}
 	var PackIdMessage message.Message
-	err = json.Unmarshal(result, &PackIdMessage)
+	err = PackIdMessage.UnmarshalJSON(result)
 	if err != nil {
 		logger.Error("ChoosePackTimerFunc Error trying to UnMarshal NotDesiredPack response")
 	}
