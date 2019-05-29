752e91b0fbb45e9aee5720b15ebafffb851cb14c
Uimin Maksim <uimin1maksim@yandex.ru>
2019-05-29 10:51:50 +0300

    Secret encrypted

--- chat_microservice/run.sh
+++ chat_microservice/run.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+#current_dir=$(pwd)
+#cd ${BASEPATH}/chat_microservice
+#go build -tags netgo -a -v .
+#cd ${current_dir}
+
+#image="sleepless_nights_chat_microservice"
+#docker build ${BASEPATH}/chat_microservice/. -t ${image}
+
 name="chat_ms"
 container=$(docker run \
     -e "CONSUL_ADDR=${CONSUL_ADDR}" \
