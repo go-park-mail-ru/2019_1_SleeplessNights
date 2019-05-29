4676def4d7149cd3d57ac278ba12e0e8741ddf1f
Uimin Maksim <uimin1maksim@yandex.ru>
2019-05-29 19:54:54 +0300

    Hotfix

--- .travis/push_docker_backend.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . -t sleepless_nights_backend
-docker push uimin1maksim/sleepless_nights_backend
