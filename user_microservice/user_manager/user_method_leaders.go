804323070d2a2295477dac65104c10e203fa63e9
mac <a.larutin@yandex.ru>
2019-05-31 16:58:12 +0300

    added new logic to get leaderboard

--- user_microservice/user_manager/user_method_leaders.go
+++ user_microservice/user_manager/user_method_leaders.go
@@ -1,18 +1,22 @@
 package user_manager
 
 import (
+	"github.com/go-park-mail-ru/2019_1_SleeplessNights/shared/config"
 	"github.com/go-park-mail-ru/2019_1_SleeplessNights/shared/services"
-	"github.com/go-park-mail-ru/2019_1_SleeplessNights/user_microservice/database"
-	"github.com/jackc/pgx"
 	"golang.org/x/net/context"
+	"math"
 )
 
+var LeaderBoardLen = config.GetInt("user_ms.pkg.user_manager.board_len")
+var PageLen = uint64(config.GetInt("user_ms.pkg.user_manager.page_len"))
+
 func (us *userManager) GetLeaderBoardPage(ctx context.Context, in *services.PageData) (*services.LeaderBoardPage, error) {
-	page, err := database.GetInstance().GetUsers(in)
-	if _err, ok := err.(pgx.PgError); ok {
-		logger.Errorf("Failed to get users: %v", err.Error())
-		err = handlerError(_err)
-		return nil, err
+	var page *services.LeaderBoardPage
+	for i := in.Since*PageLen - PageLen; i < in.Since*PageLen-1; i++ {
+		page.Leaders = append(page.Leaders, profiles[i])
+	}
+	if len(profiles) < LeaderBoardLen {
+		page.PagesCount = uint64(math.Ceil(float64(len(profiles)) / float64(PageLen)))
 	}
 	return page, nil
 }
