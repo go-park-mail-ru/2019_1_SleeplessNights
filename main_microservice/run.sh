5a69e0670ea79ca5e590696036a1206b19dabe31
Uimin Maksim <uimin1maksim@yandex.ru>
2019-06-01 15:33:33 +0300

    Added disc sync for DB and static

--- main_microservice/run.sh
+++ main_microservice/run.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
+#Разбираем параметры командной строки
+static="${BASEPATH}/main_microservice/static"
+while [[ -n "$1" ]]
+do
+case "$1" in
+--prod) static="/home/ubuntu/Deploy/Frontend/public/assets";;
+*) return 1;;
+esac
+shift
+done
+
 name="main_ms"
 container=$(docker run \
     -e "CONSUL_ADDR=${CONSUL_ADDR}" \
     --name ${name} --rm \
     --network host \
+    -v ${BASEPATH}/main_microservice/static:${static} \
     -d ${BACKEND_IMAGE} go run /server/main_microservice/main.go);
 
 echo "Main-MS container: ${container}"
--- user_microservice/run.sh
+++ user_microservice/run.sh
@@ -4,6 +4,6 @@ container=$(docker run \
     -e "CONSUL_ADDR=${CONSUL_ADDR}" \
     --name ${name} --rm \
     --network host \
-    ${BACKEND_IMAGE} go run /server/user_microservice/main.go);
+    -d ${BACKEND_IMAGE} go run /server/user_microservice/main.go);
 
 echo "User-MS container: ${container}"
