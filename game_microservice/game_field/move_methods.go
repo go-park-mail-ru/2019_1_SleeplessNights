f671d5d487bfff2fd5295293c6bcadd9d1151c96
Max <vpupkin9208@gmail.com>
2019-06-02 05:55:55 +0300

    Seconds Fix

--- game_microservice/game_field/move_methods.go
+++ game_microservice/game_field/move_methods.go
@@ -7,7 +7,6 @@ import (
 	"github.com/go-park-mail-ru/2019_1_SleeplessNights/game_microservice/database"
 	"github.com/go-park-mail-ru/2019_1_SleeplessNights/game_microservice/event"
 	"github.com/go-park-mail-ru/2019_1_SleeplessNights/game_microservice/message"
-	"time"
 )
 
 func isPrizePosition(x, y int) bool {
@@ -164,7 +163,7 @@ func (gf *GameField) tryMovePlayer(player *gfPlayer, nextX int, nextY int) (e []
 	gf.regQuestion = *(gf.GetQuestionByCell(nextX, nextY))
 	payload := struct {
 		Question database.Question
-		Time     time.Duration
+		Time     int
 	}{
 		Question: gf.regQuestion,
 		Time:     TurnDuration,
